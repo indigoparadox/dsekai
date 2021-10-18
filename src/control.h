c1b13e1f76065d9563644d4ebc98e2d436764019
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-10-18 18:39:20 -0400

    More docs, docs formatting.

--- src/control.h
+++ src/control.h
@@ -2,35 +2,76 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 
+/*! \file control.h
+ *  \brief Tools for drawing and interacting with graphical UI elements.
+ */
+
 #define CONTROL_COUNT_MAX           20
 
+/**
+ * \brief CONTROL::type used to display static text.
+ *
+ * Requires a ::MEMORY_HANDLE containing the string to display to be attached to
+ * the accompanying CONTROL::data member.
+ */
 #define CONTROL_TYPE_LABEL          0
+/*! \brief CONTROL::type used to indicate a potential action. */
 #define CONTROL_TYPE_BUTTON         1
+/*! \brief CONTROL::type used to indicate a selectable option. */
 #define CONTROL_TYPE_CHECK          2
+/**
+ * \brief CONTROL::type used to display a sprite asset.
+ *
+ * Requires a ::RESOURCE_ID identifying the sprite to display to be attached to
+ * the accompanying CONTROL::data member.
+ */
 #define CONTROL_TYPE_SPRITE         3
 
+/*! \brief CONTROL::status indicating control is hidden/inactive. */
 #define CONTROL_STATE_NONE          0
+/*! \brief CONTROL::status indicating control is visible and interactive. */
 #define CONTROL_STATE_ENABLED       1
+/*! \brief CONTROL::status indicating control has cursor focus. */
 #define CONTROL_STATE_ACTIVE        2
+/*! \brief CONTROL::status indicating control option has been selected. */
 #define CONTROL_STATE_CHECKED       3
 
+/*! \brief A piece of data attached to CONTROL::data. */
 union CONTROL_DATA {
    uint32_t scalar;
    MEMORY_HANDLE handle;
+   /*! \brief Identifier for an asset (e.g. for CONTROL_TYPE_SPRITE). */
    RESOURCE_ID res_id;
 };
 
+/*! \brief A graphical UI element, typically attached to a WINDOW. */
 struct CONTROL {
+   /*! \brief Unique identifier used to find this control in a window stack. */
    uint32_t id;
+   /*! \brief Type of control defined by a constant. */
    uint16_t type;
+   /*! \brief Current drawing status of this control. */
    uint16_t status;
-   int16_t x; /* Relative to containing window. */
-   int16_t y; /* Relative to containing window. */
+   /**
+    * \brief The left horizontal offset of the control in pixels.
+    *
+    * Relative to containing window.
+    */
+   int16_t x;
+   /**
+    * \brief The top vertical offset of the control in pixels.
+    *
+    * Relative to containing window.
+    */
+   int16_t y;
+   /*! \brief The width of the control in pixels. */
    int16_t w;
+   /*! \brief The height of the control in pixels. */
    int16_t h;
    int8_t scale;
    GRAPHICS_COLOR fg;
    GRAPHICS_COLOR bg;
+   /*! \brief Data used to draw a control (e.g. text or sprite to display). */
    union CONTROL_DATA data;
 };
 
--- src/window.h
+++ src/window.h
@@ -2,46 +2,89 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+/*! \file window.h
+ *  \brief Tools for drawing and interacting with graphical windows on-screen.
+ */
+
 #define WINDOW_CENTERED -1
 
 #define WINDOW_COUNT_MAX 10
 
+/*! \brief WINDOW::status indicating window is hidden/inactive. */
 #define WINDOW_STATUS_EMPTY    0
+/*! \brief WINDOW::status indicating window is being animated open. */
 #define WINDOW_STATUS_OPENING  1
+/*! \brief WINDOW::status indicating window is visible normally. */
 #define WINDOW_STATUS_VISIBLE  2
+/*! \brief WINDOW::status indicating window is blocking all input. */
 #define WINDOW_STATUS_MODAL  3
 
+/**
+ * \brief Defines a graphical window frame/background with a set of image
+ *        assets.
+ */
 struct WINDOW_FRAME {
+   /*! \brief Top-right corner. */
    RESOURCE_ID tr;
+   /*! \brief Top-left corner. */
    RESOURCE_ID tl;
+   /*! \brief Bottom-right corner. */
    RESOURCE_ID br;
+   /*! \brief Bottom-left corner. */
    RESOURCE_ID bl;
+   /*! \brief Top edge. */
    RESOURCE_ID t;
+   /*! \brief Bottom edge. */
    RESOURCE_ID b;
+   /*! \brief Right edge. */
    RESOURCE_ID r;
+   /*! \brief Left edge. */
    RESOURCE_ID l;
+   /*! \brief Center fill. */
    RESOURCE_ID c;
 };
 
 struct WINDOW {
+   /*! \brief Unique identifier used to find this window in the global stack. */
    uint32_t id;
+   /*! \brief Current drawing status of this window. */
    uint8_t status;
+   /*! \brief 1 indicates window must be redrawn, 0 otherwise. */
    uint8_t dirty;
    uint8_t frame_idx;
+   /*! \brief Number of controls in WINDOW::controls_handle. */
    uint8_t controls_count;
+   /*! \brief List of CONTROL to draw inside this window. */
    MEMORY_HANDLE controls_handle;
+   /*! \brief The left horizontal offset of the window in pixels. */
    int16_t x;
+   /*! \brief The top vertical offset of the window in pixels. */
    int16_t y;
+   /*! \brief The width of the window in pixels. */
    int16_t w;
+   /*! \brief The height of the window in pixels. */
    int16_t h;
 };
 
 void window_init();
 void window_shutdown();
-int window_draw_all( struct DSEKAI_STATE* );
+int window_draw_all( struct DSEKAI_STATE* state );
+
+/**
+ * \brief Push a new window onto the global window stack.
+ * \param id A unique identifier for the window being pushed.
+ * \param status
+ * \param x The left horizontal offset of the window in pixels.
+ * \param y The top vertical offset of the window in pixels.
+ * \param w The width of the window in pixels.
+ * \param h The height of the window in pixels.
+ * \param frame_idx The index of the window frame to use.
+ * \param state ::MEMORY_PTR to the global engine state.
+ */
 int16_t window_push(
-   uint32_t, uint8_t,
-   int16_t, int16_t, int16_t, int16_t, uint8_t, struct DSEKAI_STATE* );
+   uint32_t id, uint8_t status,
+   int16_t x, int16_t y, int16_t w, int16_t h, uint8_t frame_idx,
+   struct DSEKAI_STATE* state
 void window_pop( uint32_t, struct DSEKAI_STATE* );
 int16_t window_modal( struct DSEKAI_STATE* );
 
