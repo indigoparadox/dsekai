392494942504107d24c338ab7d02f8354e08737f
indigoparadox <indigo.repo@interfinitydynamics.info>
2022-06-14 00:22:51 -0400

    Build package if not built and trying to run.

--- pkgs.sh
+++ pkgs.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
+build_sdl() {
+   make -f Makefile.sdl DTHRESHOLD=$DEBUG_THRESHOLD BUILD=$BUILD pkg_sdl || exit
+   make -f Makefile.sdl DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_ASN=TRUE BUILD=$BUILD pkg_sdl || exit
+   make -f Makefile.sdl DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_JSON=TRUE BUILD=$BUILD pkg_sdl || exit
+}
+
+build_dos() {
+   make -f Makefile.dos DTHRESHOLD=$DEBUG_THRESHOLD RESOURCE=FILE FMT_ASN=TRUE BUILD=$BUILD ARCFMT=ZIP pkg_dos || exit
+}
+
+build_win() {
+   make -f Makefile.win16 DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_ASN=TRUE BUILD=$BUILD ARCFMT=ZIP pkg_win16 || exit
+   make -f Makefile.win32 DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_JSON=TRUE BUILD=$BUILD ARCFMT=ZIP pkg_win32 || exit
+}
+
 if [ "$1" = "run" ]; then
    PLAT_SPEC=$2
+   BUILD=DEBUG
+   DEBUG_THRESHOLD=1
    
    if [ "$PLAT_SPEC" = "sdl" ]; then
-      cd pkgbuild/dsekai-sdl-*vga*-asn && ./dsekai
+      if [ ! -d pkgbuild/dsekai-sdl-*-vga-*-asn ]; then
+         build_sdl
+      fi
+      cd pkgbuild/dsekai-sdl-*-vga-*-asn && ./dsekai
    elif [ "$PLAT_SPEC" = "dos" ]; then
+      if [ ! -d pkgbuild/dsekai-dos-*-cga-*-asn ]; then
+         build_dos
+      fi
       # This is complicated by FreeDOS for /D not finding wildcards...
       # Requires a symlink "dsekai" to this directory in dosemu root.
       if [ ! -L "dsdos" ]; then
@@ -15,9 +38,15 @@ if [ "$1" = "run" ]; then
       echo "dsekai.exe" >> dsdos/dsdos.bat
       dosemu -2 "dsekai\\dsdos\\dsdos.bat"
    elif [ "$PLAT_SPEC" = "win32" ]; then
-      cd pkgbuild/dsekai-win32-*vga*-json && wine ./dsekai32.exe
+      if [ ! -d pkgbuild/dsekai-win32-*-vga-*-json ]; then
+         build_win
+      fi
+      cd pkgbuild/dsekai-win32-*-vga-*-json && wine ./dsekai32.exe
    elif [ "$PLAT_SPEC" = "win16" ]; then
-      cd pkgbuild/dsekai-win16-*vga*-asn && wine ./dsekai16.exe
+      if [ ! -d pkgbuild/dsekai-win16-*-vga-*-asn ]; then
+         build_win
+      fi
+      cd pkgbuild/dsekai-win16-*-vga-*-asn && wine ./dsekai16.exe
    fi
    
    exit
@@ -31,9 +60,7 @@ else
 fi
 
 if [ "$PLAT_SPEC" = "sdl" ] || [ -z "$PLAT_SPEC" ]; then
-   make -f Makefile.sdl DTHRESHOLD=$DEBUG_THRESHOLD BUILD=$BUILD pkg_sdl || exit
-   make -f Makefile.sdl DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_ASN=TRUE BUILD=$BUILD pkg_sdl || exit
-   make -f Makefile.sdl DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_JSON=TRUE BUILD=$BUILD pkg_sdl || exit
+   build_sdl
 fi
 
 if [ "$PLAT_SPEC" = "palm" ] || [ -z "$PLAT_SPEC" ]; then
@@ -45,12 +72,11 @@ if [ "$PLAT_SPEC" = "xlib" ] || [ -z "$PLAT_SPEC" ]; then
 fi
 
 if [ "$PLAT_SPEC" = "dos" ] || [ -z "$PLAT_SPEC" ]; then
-   make -f Makefile.dos DTHRESHOLD=$DEBUG_THRESHOLD RESOURCE=FILE FMT_ASN=TRUE BUILD=$BUILD ARCFMT=ZIP pkg_dos || exit
+   build_dos
 fi
 
 if [ "$PLAT_SPEC" = "win" ] || [ -z "$PLAT_SPEC" ]; then
-   make -f Makefile.win16 DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_ASN=TRUE BUILD=$BUILD ARCFMT=ZIP pkg_win16 || exit
-   make -f Makefile.win32 DTHRESHOLD=$DEBUG_THRESHOLD DEPTH=VGA RESOURCE=FILE FMT_JSON=TRUE BUILD=$BUILD ARCFMT=ZIP pkg_win32 || exit
+   build_win
 fi
 
 if [ "$PLAT_SPEC" = "wasm" ] || [ -z "$PLAT_SPEC" ]; then
